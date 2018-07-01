5f382293b1095cf8b88aabbeb08eb8a71f43bee6
Zhou, Keqin <kezhou@microstrategy.com>
2018-07-01 16:36:21 +0800

    DE0008

--- src/Reload.js
+++ src/Reload.js
@@ -3,28 +3,59 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Input } from 'antd';
 import { Button, Icon } from 'antd';
-
+import { DIRECTOR } from './common/config'
+import { Modal } from 'antd'
 
 class Reload extends Component {
     constructor() {
         super();
+        this.state = { visible: false }
     }
-    handleResetContent = (e) => {
+
+    showModal = () => {
+        this.setState({
+            visible: true,
+        });
+    }
+
+    handleOk = (e) => {
         this.props.reloadContent(this.props.room)
         this.props.resetMask(this.props.room)
+        this.setState({
+            visible: false,
+        });
+    }
+
+    handleCancel = (e) => {
+        this.setState({
+            visible: false,
+        });
     }
 
     render() {
+        let element = null
+        if (this.props.role == DIRECTOR) {
+            element = <Button type="primary" icon="reload" onClick={this.showModal.bind(this)} className='upload' />
+        }
         return (
             <div>
-                <Button type="primary" icon="reload" onClick={this.handleResetContent.bind(this)} className='upload'/>
+                {element}
+                <Modal
+                    title="Warning"
+                    visible={this.state.visible}
+                    onOk={this.handleOk}
+                    onCancel={this.handleCancel}
+                >
+                    <p>You will refresh the content.</p>
+                </Modal>
             </div>)
     }
 }
 
 const mapStateToProps = (state) => {
     return {
-        room: state.get('userConfig').room
+        room: state.get('userConfig').room,
+        role: state.get('userConfig').role
     }
 }
 
